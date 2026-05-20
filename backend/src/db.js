b4f46bbc422dccc0a4de20b0b161195c606a9e74
Anzhela-Ostrovska1 <anzhelk451964@gmail.com>
2026-05-20 16:51:56 -0700

    updated db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -19,15 +19,6 @@ const sequelize = new Sequelize({
   }
 });
 
-// const sequelize = new Sequelize({
-//   dialect: "postgres",
-//   database: process.env.DB_NAME,
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   logging: false,
-// });
 
 sequelize.authenticate()
   .then(() => console.log("✅ Connected to PostgreSQL via Sequelize"))
