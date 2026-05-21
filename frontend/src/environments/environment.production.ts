fdc524027e5b8b2f4cbb669a8770bc14f09bf3f8
Anzhela-Ostrovska1 <anzhelk451964@gmail.com>
2026-05-21 14:37:50 -0700

    fixed apiURL

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-   apiUrl: 'https://expense-tracker-backend-quz9.onrender.com'
-};
+apiUrl: 'https://expense-tracker-backend-quz9.onrender.com/api'
+}
