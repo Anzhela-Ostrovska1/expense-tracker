bb9655cf133d65ca4775e8367e547a1431a5b500
Anzhela-Ostrovska1 <anzhelk451964@gmail.com>
2026-05-23 22:57:49 -0700

    updated server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,9 +22,13 @@ app.use(express.json());
 app.use("/api/auth", authRoutes); 
 app.use("/api/transactions", transactionRoutes); 
 
-app.get("/api/test", (req, res) => {
-  res.json({ message: "Backend works 🚀" });
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
 });
+
+// app.get("/api/test", (req, res) => {
+//   res.json({ message: "Backend works 🚀" });
+// });
 
 const PORT = process.env.PORT || 5000;    
 app.listen(PORT, () => {
